07cb63d667673a20be59ebe5bac1f040b49c86d9
luoluoluo22 <1137583371@qq.com>
2024-09-10 18:05:16 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# 启动 app.py
-python app.py &
+# 启动 Flask 应用
+echo "Starting Flask app..."
+gunicorn -b 0.0.0.0:5000 app:app &
 
-# 启动 admin.py
-python admin.py &
+# 启动管理员 Flask 应用
+echo "Starting admin Flask app..."
+gunicorn -b 0.0.0.0:5002 admin:app
+
+# 注意：这里我们假设 gunicorn 是安装在 requirements.txt 中的依赖
